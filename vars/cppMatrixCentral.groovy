7d53e3015dd1d75e36a381924930bf2f312d0d1f
Gerar <Gerar1928@gmail.com>
2022-03-25 23:42:45 -0400

    set artifact path

--- vars/cppMatrixCentral.groovy
+++ vars/cppMatrixCentral.groovy
@@ -67,7 +67,7 @@ def call(Map pipelineParams) {
                                     if (env.PROJECT == "iosqueak") {
                                         echo 'Unarchiving dependencies needed...'
                                         
-                                        unarchive mapping: ["mpm-artifacts/jenkins/": 'arctic-tern, libdeps']
+                                        unarchive mapping: ["jenkins/": 'arctic-tern, libdeps']
 
                                         sh 'tar -xzvf *.tar.gz'
                                     } else {
