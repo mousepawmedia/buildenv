399fbcb3a57c3a71804e8b98430a7f007eef0a44
Gerar1928 <gerar1928@gmail.com>
2022-06-08 21:34:33 -0400

    created copyArchives.groovy
    Summary: Created copyArchives.groovy in order to refactor pipelines and avoid repeating code.
    
    Test Plan: N/A
    
    Reviewers: jcmcdonald
    
    Reviewed By: jcmcdonald
    
    Revert Plan: N/A
    
    Differential Revision: https://phab.mousepawmedia.com/D499

---
+++ vars/copyArchives.groovy
@@ -0,0 +1,29 @@
+def call(Map pipelineParams) {
+    script {
+        // check if file exists
+        def containsDeps = sh(script: "test -f ${pipelineParams.directory}/dependencies.txt && echo true || echo false", returnStdout: true)
+
+        if (containsDeps.contains('true')) {
+            echo 'Unarchiving dependencies needed...'
+
+            // read file content to store it on a variable
+            def deps_str = sh(script: "cat ${pipelineParams.directory}/dependencies.txt", returnStdout: true)
+
+            // convert deps_str to an array
+            def deps_arr = deps_str.trim().split(',')
+
+            for (int i = 0; i < deps_arr.size(); ++i) {
+                // copy artifacts from last succesful build
+                copyArtifacts projectName: "${deps_arr[i]}_central"
+                target: pipelineParams.target
+
+                sh "cd ${deps_arr[i]} && \
+                tar -xzvf *.tar.gz"
+            }
+        }
+                                        
+        if (containsDeps.contains('false')) {
+            echo 'This project does not have dependencies to unarchive'
+        }
+    }
+}
--- vars/cppMatrixCentral.groovy
+++ vars/cppMatrixCentral.groovy
@@ -63,33 +63,10 @@ def call(Map pipelineParams) {
                         }
                         stage('Copy Archive') {
                             steps {
-                                script {
-                                    // check if file exists
-                                    def containsDeps = sh(script: "test -f ${env.PROJECT}/dependencies_central.txt && echo true || echo false", returnStdout: true)
-
-                                    if (containsDeps.contains('true')) {
-                                        echo 'Unarchiving dependencies needed...'
-
-                                        // read file content to store it on a variable
-                                        def deps_str = sh(script: "cat ${env.PROJECT}/dependencies_central.txt", returnStdout: true)
-
-                                        // convert deps_str to an array
-                                        def deps_arr = deps_str.trim().split(',')
-
-                                        for (int i = 0; i < deps_arr.size(); ++i) {
-                                            // copy artifacts from last succesful build
-                                            copyArtifacts projectName: "${deps_arr[i]}_central"
-                                            target: "workspace/${OS}/${COMPILER}"
-
-                                            sh "cd ${deps_arr[i]} && \
-                                            tar -xzvf *.tar.gz"
-                                        }
-                                    }
-                                    
-                                    if (containsDeps.contains('false')) {
-                                        echo 'This project does not have dependencies to unarchive'
-                                    }
-                                }
+                                copyArchives(
+                                    'directory': env.PROJECT,
+                                    'target': "workspace/${OS}/${COMPILER}"
+                                )
                             }
                         }
                         stage('Build') {
