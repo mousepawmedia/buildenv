19bed5eb4880784a2d793fd8de2984b5e8449290
Gerar1928 <gerar1928@gmail.com>
2022-06-23 21:24:51 -0400

    ci: added script to test docker within the jenkins agent

--- vars/buildAndPushDockerfiles.groovy
+++ vars/buildAndPushDockerfiles.groovy
@@ -18,73 +18,85 @@ def call(Map pipelineParams) {
                     }
                 }
                 stages {
-                    stage('Checkout') {
+                    // stage('Checkout') {
+                    //     steps {
+                    //         checkoutStep(
+                    //             'repo': env.REPO,
+                    //             'branch': params.BRANCH,
+                    //             'directory': 'target',
+                    //             'diff_id': '',
+                    //             'revision_id': ''
+                    //         )
+                    //     }
+                    // }
+                    stage('Install Docker') {
                         steps {
-                            checkoutStep(
-                                'repo': env.REPO,
-                                'branch': params.BRANCH,
-                                'directory': 'target',
-                                'diff_id': '',
-                                'revision_id': ''
-                            )
+                            sh 'sudo apt-get install ca-certificates curl gnupg lsb-release'
+                            sh 'sudo mkdir -p /etc/apt/keyrings'
+                            sh 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg'
+                            sh 'echo \
+                                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+                                $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
+                            sh 'sudo apt-get update'
+                            sh 'sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin'
                         }
                     }
-                    stage('Login') {
-                        steps {
+                    // stage('Login') {
+                    //     steps {
 
-                            sh 'printenv'
+                    //         sh 'printenv'
 
-                            sh 'docker login registry.mousepawmedia.com'
-                        }
-                    }
-                    stage('Build & Push mpm-* Dockerfiles') {
-                        matrix {
-                            axes {
-                                axis {
-                                    name 'DOCKERFILE'
-                                    values 'bionic', 'focal'
-                                }
-                            }
-                            stages {
-                                stage('Build') {
-                                    steps {
-                                        sh "cd target/dockerfiles && \
-                                        docker image build  mpm-${DOCKERFILE}"
-                                    }
-                                }
-                                stage('Publish') {
-                                    steps {
-                                        sh "cd target/dockerfiles && \
-                                        docker image push registry.mousepawmedia.com/mpm-${DOCKERFILE}:latest"
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    stage('Build & Push jenkins.mpm-* Dockerfiles') {
-                        matrix {
-                            axes {
-                                axis {
-                                    name 'DOCKERFILE'
-                                    values 'bionic', 'focal'
-                                }
-                            }
-                            stages {
-                                stage('Build') {
-                                    steps {
-                                        sh "cd target/dockerfiles && \
-                                        docker image build  jenkins.mpm-${DOCKERFILE}"
-                                    }
-                                }
-                                stage('Publish') {
-                                    steps {
-                                        sh "cd target/dockerfiles && \
-                                        docker image push registry.mousepawmedia.com/jenkins.mpm-${DOCKERFILE}:latest"
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    //         sh 'docker login registry.mousepawmedia.com'
+                    //     }
+                    // }
+                    // stage('Build & Push mpm-* Dockerfiles') {
+                    //     matrix {
+                    //         axes {
+                    //             axis {
+                    //                 name 'DOCKERFILE'
+                    //                 values 'bionic', 'focal'
+                    //             }
+                    //         }
+                    //         stages {
+                    //             stage('Build') {
+                    //                 steps {
+                    //                     sh "cd target/dockerfiles && \
+                    //                     docker image build  mpm-${DOCKERFILE}"
+                    //                 }
+                    //             }
+                    //             stage('Publish') {
+                    //                 steps {
+                    //                     sh "cd target/dockerfiles && \
+                    //                     docker image push registry.mousepawmedia.com/mpm-${DOCKERFILE}:latest"
+                    //                 }
+                    //             }
+                    //         }
+                    //     }
+                    // }
+                    // stage('Build & Push jenkins.mpm-* Dockerfiles') {
+                    //     matrix {
+                    //         axes {
+                    //             axis {
+                    //                 name 'DOCKERFILE'
+                    //                 values 'bionic', 'focal'
+                    //             }
+                    //         }
+                    //         stages {
+                    //             stage('Build') {
+                    //                 steps {
+                    //                     sh "cd target/dockerfiles && \
+                    //                     docker image build  jenkins.mpm-${DOCKERFILE}"
+                    //                 }
+                    //             }
+                    //             stage('Publish') {
+                    //                 steps {
+                    //                     sh "cd target/dockerfiles && \
+                    //                     docker image push registry.mousepawmedia.com/jenkins.mpm-${DOCKERFILE}:latest"
+                    //                 }
+                    //             }
+                    //         }
+                    //     }
+                    // }
                     stage('Clean workspace') {
                         steps {
                             sh 'rm -rf *'
