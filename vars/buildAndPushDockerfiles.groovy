3240260c7c2be1ef6e4f9ab48ccee2f1467e7302
Gerar1928 <gerar1928@gmail.com>
2022-07-19 20:34:38 -0400

    ci: testing dind image on pipelines

--- vars/buildAndPushDockerfiles.groovy
+++ vars/buildAndPushDockerfiles.groovy
@@ -12,33 +12,24 @@ def call(Map pipelineParams) {
         stages {
             stage('Build Environment') {
                 agent {
-                    node {
-                        label "mpm-focal"
-                        customWorkspace "/workspace/focal/clang"
+                    docker { 
+                        image 'docker:dind'
+                        args '--privileged -d'
                     }
                 }
                 stages {
-                    // stage('Checkout') {
-                    //     steps {
-                    //         checkoutStep(
-                    //             'repo': env.REPO,
-                    //             'branch': params.BRANCH,
-                    //             'directory': 'target',
-                    //             'diff_id': '',
-                    //             'revision_id': ''
-                    //         )
-                    //     }
-                    // }
-                    stage('Install Docker') {
+                    stage('Checkout') {
                         steps {
-                            sh 'sudo apt-get install ca-certificates curl gnupg lsb-release'
-                            sh 'sudo mkdir -p /etc/apt/keyrings'
-                            sh 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg'
-                            sh 'echo \
-                                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-                                $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
-                            sh 'sudo apt-get update'
-                            sh 'sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin'
+
+                            sh 'docker --version'
+
+                            // checkoutStep(
+                            //     'repo': env.REPO,
+                            //     'branch': params.BRANCH,
+                            //     'directory': 'target',
+                            //     'diff_id': '',
+                            //     'revision_id': ''
+                            // )
                         }
                     }
                     // stage('Login') {
@@ -97,11 +88,6 @@ def call(Map pipelineParams) {
                     //         }
                     //     }
                     // }
-                    stage('Clean workspace') {
-                        steps {
-                            sh 'rm -rf *'
-                        }
-                    }
                 }
             }
         }
