311e6c91e308f857dee0e00822a236f3dbe7bb39
Gerar1928 <gerar1928@gmail.com>
2022-10-18 19:25:38 -0400

    ci: modified script to build and push docker images

--- vars/buildAndPushImages.groovy
+++ vars/buildAndPushImages.groovy
@@ -3,6 +3,7 @@ def call(Map pipelineParams) {
         parameters {
             string(name: 'BRANCH', defaultValue: 'devel')
         }
+
         environment {
             REPO = "${pipelineParams.repo}"
         }
@@ -13,78 +14,77 @@ def call(Map pipelineParams) {
                 customWorkspace '/workspace/focal/clang'
             }
         }
-        
+
         stages {
             stage('Checkout') {
                 steps {
-
-                sh 'sudo docker images'
-
-                // checkoutStep(
-                //     'repo': env.REPO,
-                //     'branch': params.BRANCH,
-                //     'directory': 'target',
-                //     'diff_id': '',
-                //     'revision_id': ''
-                // )
+                    checkoutStep(
+                        'repo': env.REPO,
+                        'branch': params.BRANCH,
+                        'directory': 'target',
+                        'diff_id': '',
+                        'revision_id': ''
+                    )
                 }
             }
-                    // stage('Login') {
-                    //     steps {
 
-                    //         sh 'printenv'
+            stage('Login') {
+                steps {
+                    withCredentials([usernamePassword(credentialsId: '33c8e2d8-db88-4155-966f-2a818cbd2dd0', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        sh "sudo docker login -u ${USERNAME} -p ${PASSWORD} registry.mousepawmedia.com"
+                    }
+                }
+            }
 
-                    //         sh 'docker login registry.mousepawmedia.com'
-                    //     }
-                    // }
-                    // stage('Build & Push mpm-* Dockerfiles') {
-                    //     matrix {
-                    //         axes {
-                    //             axis {
-                    //                 name 'DOCKERFILE'
-                    //                 values 'bionic', 'focal'
-                    //             }
-                    //         }
-                    //         stages {
-                    //             stage('Build') {
-                    //                 steps {
-                    //                     sh "cd target/dockerfiles && \
-                    //                     docker image build  mpm-${DOCKERFILE}"
-                    //                 }
-                    //             }
-                    //             stage('Publish') {
-                    //                 steps {
-                    //                     sh "cd target/dockerfiles && \
-                    //                     docker image push registry.mousepawmedia.com/mpm-${DOCKERFILE}:latest"
-                    //                 }
-                    //             }
-                    //         }
-                    //     }
-                    // }
-                    // stage('Build & Push jenkins.mpm-* Dockerfiles') {
-                    //     matrix {
-                    //         axes {
-                    //             axis {
-                    //                 name 'DOCKERFILE'
-                    //                 values 'bionic', 'focal'
-                    //             }
-                    //         }
-                    //         stages {
-                    //             stage('Build') {
-                    //                 steps {
-                    //                     sh "cd target/dockerfiles && \
-                    //                     docker image build  jenkins.mpm-${DOCKERFILE}"
-                    //                 }
-                    //             }
-                    //             stage('Publish') {
-                    //                 steps {
-                    //                     sh "cd target/dockerfiles && \
-                    //                     docker image push registry.mousepawmedia.com/jenkins.mpm-${DOCKERFILE}:latest"
-                    //                 }
-                    //             }
-                    //         }
-                    //     }
-                    // }
+            stage('Build & Push mpm-* Dockerfiles') {
+                matrix {
+                    axes {
+                        axis {
+                            name 'DOCKERFILE'
+                            values 'bionic', 'focal', 'jammy'
+                        }
+                    }
+                    stages {
+                        stage('Build') {
+                            steps {
+                                echo "Building Dockerfiles..."
+                                sh "sudo docker build target/dockerfiles/mpm-${DOCKERFILE}"
+                            }
+                        }
+                        stage('Publish') {
+                            steps {
+                                echo "Pushing images to registry.mousepawmedia.com"
+                                sh "sudo docker image push registry.mousepawmedia.com/mpm-${DOCKERFILE}:latest"
+                            }
+                        }
+                    }
+                }
+            }
+
+            stage('Build & Push jenkins.mpm-* Dockerfiles') {
+                matrix {
+                    axes {
+                        axis {
+                            name 'DOCKERFILE'
+                            values 'bionic', 'focal', 'jammy'
+                        }
+                    }
+                    stages {
+                        stage('Build') {
+                            steps {
+                                echo "Building Dockerfiles..."
+                                sh "sudo docker build target/dockerfiles/jenkins.mpm-${DOCKERFILE}"
+                            }
+                        }
+                        stage('Publish') {
+                            steps {
+                                echo "Pushing images to registry.mousepawmedia.com"
+                                sh "sudo docker image push registry.mousepawmedia.com/jenkins.mpm-${DOCKERFILE}:latest"
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
